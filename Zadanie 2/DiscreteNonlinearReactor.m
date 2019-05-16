fcd99b9493b8ab4f504e8c15bfb39628275b9a04
macklo <maciek2608@gmail.com>
2019-05-16 11:39:56 +0200

    Zadanie 2 ;) - screeny nastawy, działanie, chlanie

--- Zadanie 2/DiscreteNonlinearReactor.m
+++ Zadanie 2/DiscreteNonlinearReactor.m
@@ -31,7 +31,7 @@ classdef DiscreteNonlinearReactor < handle
 			Ca = (F_in*input(1)/V - F*x(1)/V - k_0*exp(-E_R/x(2))*x(1))*obj.Tp + x(1);
 			T = (F_in*disturbance(1)/V - F*x(2)/V + h*k_0*exp(-E_R/x(2))*x(1)/(Ro*c_p) - a*(input(2))^(b+1)/((input(2)+ a*(input(2))^b/(2*Ro_c*c_pc))*(V*Ro*c_p))*(x(2)-disturbance(2)))*obj.Tp + x(2);
 			obj.u = [obj.u; obj.currentU];
-			obj.y = [obj.y; Ca T];
+			obj.y = [obj.y; Ca, T];
 			obj.t = [obj.t; obj.t(end) + obj.Tp];
 		end
 	end
